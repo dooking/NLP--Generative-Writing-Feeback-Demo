17d21c850935da6e84104fc0018b522bcf02eded
dooking <123456ghghgh@naver.com>
2022-06-04 01:34:44 +0900

    build

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,20 +1,15 @@
 import Image from 'next/image';
-import Link from 'next/Link';
 import LogoImage from 'public/logo.jpg';
 import * as S from './style';
 
 const Header = () => {
   return (
     <S.HeaderContainer>
-      <Link href="/">
-        <S.WaveImageBox>
-          <Image src={LogoImage} />
-        </S.WaveImageBox>
-      </Link>
+      <S.WaveImageBox>
+        <Image src={LogoImage} />
+      </S.WaveImageBox>
       <S.Navbar>
-        <Link href="/demo">
-          <S.NavbarLink>Demo</S.NavbarLink>
-        </Link>
+        <S.NavbarLink>Demo</S.NavbarLink>
         <S.NavbarLink>Team</S.NavbarLink>
       </S.Navbar>
     </S.HeaderContainer>
